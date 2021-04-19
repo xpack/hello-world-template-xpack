42c5d406c0bd30006005fc6890b437e639c2048d
Liviu Ionescu <ilg@livius.net>
2021-04-19 13:42:36 +0300

    update license tests

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@
  * This file is part of the xPack project (http://xpack.github.io).
  * Copyright (c) 2021 Liviu Ionescu. All rights reserved.
  *
- * This Source Code File is subject to the MIT License terms.
+ * This Software is provided under the terms of the MIT License.
  * If a copy of the license was not distributed with this file, it can
  * be obtained from https://opensource.org/licenses/MIT/.
  */
