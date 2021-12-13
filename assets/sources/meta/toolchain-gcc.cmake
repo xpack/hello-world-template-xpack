2dfba0b8f1c2d40061babecbe40ee998d1f78a51
Liviu Ionescu <ilg@livius.net>
2021-12-13 09:25:46 +0200

    add workaround for Windows .cmd issue

--- assets/sources/meta/toolchain-gcc.cmake
+++ assets/sources/meta/toolchain-gcc.cmake
@@ -24,79 +24,89 @@ set(CMAKE_SIZE "${triple}size")
 
 # -----------------------------------------------------------------------------
 
+# TODO: remove workaround once VS Code is fixed.
+# VS Code does not properly identify the shims used by npm/xpm,
+# thus make the extension explicit.
+if("${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "Windows")
+  set(CMAKE_C_COMPILER "${CMAKE_C_COMPILER}.cmd")
+  set(CMAKE_CXX_COMPILER "${CMAKE_CXX_COMPILER}.cmd")
+endif()
+
+# -----------------------------------------------------------------------------
+
 # TODO: understand why CMAKE_<lang>_OUTPUT_EXTENSION is not effective
 
-set(CMAKE_ASM_FLAGS " -x assembler-with-cpp" 
+set(CMAKE_ASM_FLAGS " -x assembler-with-cpp"
   CACHE STRING "Flags used by the ASM compiler during all build types.")
 
-set(CMAKE_ASM_FLAGS_DEBUG "-O0 -g3" 
+set(CMAKE_ASM_FLAGS_DEBUG "-O0 -g3"
   CACHE STRING "Flags used by the ASM compiler during DEBUG builds.")
 
-set(CMAKE_ASM_FLAGS_MINSIZEREL "-Os -DNDEBUG" 
+set(CMAKE_ASM_FLAGS_MINSIZEREL "-Os -DNDEBUG"
   CACHE STRING "Flags used by the ASM compiler during MINSIZEREL builds.")
 
-set(CMAKE_ASM_FLAGS_RELEASE "-O3 -DNDEBUG" 
+set(CMAKE_ASM_FLAGS_RELEASE "-O3 -DNDEBUG"
   CACHE STRING "Flags used by the ASM compiler during RELEASE builds.")
 
-set(CMAKE_ASM_FLAGS_RELWITHDEBINFO "-O2 -g3 -DNDEBUG" 
+set(CMAKE_ASM_FLAGS_RELWITHDEBINFO "-O2 -g3 -DNDEBUG"
   CACHE STRING "Flags used by the ASM compiler during RELWITHDEBINFO builds.")
 
 # Not effective.
 set(CMAKE_ASM_OUTPUT_EXTENSION ".o")
 
-set(CMAKE_C_FLAGS 
+set(CMAKE_C_FLAGS
   CACHE STRING "Flags used by the C compiler during all build types.")
 
-set(CMAKE_C_FLAGS_DEBUG "-O0 -g3" 
+set(CMAKE_C_FLAGS_DEBUG "-O0 -g3"
   CACHE STRING "Flags used by the C compiler during DEBUG builds.")
 
-set(CMAKE_C_FLAGS_MINSIZEREL "-Os -DNDEBUG" 
+set(CMAKE_C_FLAGS_MINSIZEREL "-Os -DNDEBUG"
   CACHE STRING "Flags used by the C compiler during MINSIZEREL builds.")
 
-set(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG" 
+set(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG"
   CACHE STRING "Flags used by the C compiler during RELEASE builds.")
 
-set(CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g3 -DNDEBUG" 
+set(CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g3 -DNDEBUG"
   CACHE STRING "Flags used by the C compiler during RELWITHDEBINFO builds.")
 
 # Not effective.
 set(CMAKE_C_OUTPUT_EXTENSION ".o")
 
-set(CMAKE_CXX_FLAGS 
+set(CMAKE_CXX_FLAGS
   CACHE STRING "Flags used by the CXX compiler during all build types.")
 
-set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g3" 
+set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g3"
   CACHE STRING "Flags used by the CXX compiler during DEBUG builds.")
 
-set(CMAKE_CXX_FLAGS_MINSIZEREL "-Os -DNDEBUG" 
+set(CMAKE_CXX_FLAGS_MINSIZEREL "-Os -DNDEBUG"
   CACHE STRING "Flags used by the CXX compiler during MINSIZEREL builds.")
 
-set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG" 
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG"
   CACHE STRING "Flags used by the CXX compiler during RELEASE builds.")
 
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g3 -DNDEBUG" 
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g3 -DNDEBUG"
   CACHE STRING "Flags used by the CXX compiler during RELWITHDEBINFO builds.")
 
 # Not effective.
 set(CMAKE_CXX_OUTPUT_EXTENSION ".o")
 # set(CMAKE_CXX_OUTPUT_EXTENSION_REPLACE 1)
 
-set(CMAKE_EXE_LINKER_FLAGS 
+set(CMAKE_EXE_LINKER_FLAGS
   CACHE STRING "Flags used by the linker during all build types.")
 
 # Disabled, since apparently CMake already uses the C/CXX flags,
 # and defining them here will double them.
 if(0)
-set(CMAKE_EXE_LINKER_FLAGS_DEBUG "-O0 -g3" 
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "-O0 -g3"
   CACHE STRING "Flags used by the linker during DEBUG builds.")
 
-set(CMAKE_EXE_LINKER_FLAGS_MINSIZEREL "-Os" 
+set(CMAKE_EXE_LINKER_FLAGS_MINSIZEREL "-Os"
   CACHE STRING "Flags used by the linker during MINSIZEREL builds.")
 
-set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-O3" 
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-O3"
   CACHE STRING "Flags used by the linker during RELEASE builds.")
 
-set(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "-O2 -g3" 
+set(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "-O2 -g3"
   CACHE STRING "Flags used by the linker during RELWITHDEBINFO builds.")
 endif()
 
