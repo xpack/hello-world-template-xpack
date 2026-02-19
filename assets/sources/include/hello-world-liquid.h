5b171f669f3aaf4958277efc79f85d1d7d377659
Liviu Ionescu <ilg@livius.net>
2026-02-20 01:29:35 +0200

    assets: make more liquid

--- assets/sources/include/hello-world-liquid.h
+++ assets/sources/include/hello-world-liquid.h
@@ -1,10 +1,10 @@
 /*
  * This file is part of the xPack project (http://xpack.github.io).
- * Copyright (c) 2021 Liviu Ionescu. All rights reserved.
+ * Copyright (c) 2021-{{ year }} Liviu Ionescu. All rights reserved.
  *
  * This Software is provided under the terms of the MIT License.
  * If a copy of the license was not distributed with this file, it can
- * be obtained from https://opensource.org/licenses/MIT/.
+ * be obtained from https://opensource.org/licenses/mit.
  */
 
 #ifndef HELLO_WORLD_H_
